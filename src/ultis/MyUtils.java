24f7cc3687ee05c93b9fe867db89cdf0b7d03ef6
Thinh Tran <thinhuit@gmail.com>
2017-03-19 14:01:45 +0700

    #7: fix stupid bug

--- src/ultis/MyUtils.java
+++ src/ultis/MyUtils.java
@@ -16,14 +16,12 @@ public class MyUtils {
     public static final String ATT_NAME_CONNECTION = "ATTRIBUTE_FOR_CONNECTION";
     private static final String ATT_NAME_USER_NAME = "ATTRIBUTE_FOR_STORE_USER_NAME_IN_COOKIE";
 
-``
     public static void storeConnection(ServletRequest request, Connection conn) {
         request.setAttribute(ATT_NAME_CONNECTION, conn);
     }
 
     public static Connection getStoredConnection(ServletRequest request) {
-        Connection conn = (Connection) request.getAttribute(ATT_NAME_CONNECTION);
-        return conn;
+        return  (Connection) request.getAttribute(ATT_NAME_CONNECTION);
     }
 
     public static void storeLoginedUser(HttpSession session, UserAccount user){
