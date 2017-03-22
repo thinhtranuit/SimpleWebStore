4cf2a1c26f2d59cac940b5ae27f0111a3ac74139
Thinh Tran <thinhuit@gmail.com>
2017-03-22 23:34:20 +0700

    #10: sql file

---
+++ web/WEB-INF/Database/store.sql
@@ -0,0 +1,50 @@
+create database store;
+use store;
+
+-- Create table
+create table USER_ACCOUNT
+(
+USER_NAME VARCHAR(30) not null,
+GENDER    VARCHAR(1) not null,
+PASSWORD  VARCHAR(30) not null,
+primary key (USER_NAME)
+);
+ 
+-- Create table
+create table PRODUCT
+(
+CODE  VARCHAR(20) not null,
+NAME  VARCHAR(128) not null,
+PRICE FLOAT not null,
+primary key (CODE)
+) ;
+ 
+-- Insert data: ---------------------------------------------------------------
+ 
+insert into user_account (USER_NAME, GENDER, PASSWORD)
+values ('tom', 'M', 'tom001');
+ 
+insert into user_account (USER_NAME, GENDER, PASSWORD)
+values ('jerry', 'M', 'jerry001');
+
+insert into user_account (USER_NAME, GENDER, PASSWORD)
+values ('thinh', 'M', 'thinhdepchai');
+ 
+insert into product (CODE, NAME, PRICE)
+values ('P001', 'Java Core', 100);
+ 
+insert into product (CODE, NAME, PRICE)
+values ('P005', 'JavaScript: The Good Parts', 25);
+
+insert into product (CODE, NAME, PRICE)
+values ('P002', 'C# Core', 90);
+insert into product (CODE, NAME, PRICE)
+values ('P004', 'Code Complete', 40);
+insert into product (CODE, NAME, PRICE)
+values ('P003', 'Head First Servlets and JSP', 36);
+
+
+
+select * from user_account;
+select * from product;
+delete from product where code = 'IT001';
